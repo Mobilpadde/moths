99c8c766ec9d7adfcaf0813b8c50108a5ec490f1
Mads Cordes <Mobilpadde@gmail.com>
2023-01-11 21:59:44 +0100

    Move pieces to make sense

--- moths/otp/config.go
+++ moths/otp/config.go
@@ -9,14 +9,6 @@ type OTP struct {
 	emojies emojies.Emojies
 }
 
-func (otp OTP) Validate(moth string) bool {
-	return otp.emojies.String() == moth
-}
-
-func (otp OTP) ValidateToken(code string) bool {
-	return otp.token == code
-}
-
 func (otp OTP) Token() string {
 	return otp.token
 }
@@ -31,4 +23,12 @@ func (otp OTP) SpacedString() string {
 
 func (otp OTP) Slice() []string {
 	return otp.emojies.Slice()
+}
+
+func (otp OTP) Validate(moth string) bool {
+	return otp.String() == moth
+}
+
+func (otp OTP) ValidateToken(code string) bool {
+	return otp.Token() == code
 }
--- moths/otp/new.go
+++ moths/otp/new.go
@@ -14,14 +14,14 @@ func NewOTP(token string, amount int, em emojies.Emojies) (OTP, error) {
 	emojiAmount := len(em)
 	size := EmojiBytes * amount
 
+	moth := new(strings.Builder)
+	moth.Grow(size)
+	buffer := bufferFromToken(token)
+
 	// https://github.com/tilaklodha/google-authenticator
 	mask := 2<<uint32(31-bits.LeadingZeros32(uint32(emojiAmount-1|1))) - 1
 	step := int(math.Ceil(1.6 * float64(mask*size) / float64(emojiAmount)))
 
-	moth := new(strings.Builder)
-	moth.Grow(size)
-
-	buffer := bufferFromToken(token)
 loop:
 	for {
 		randomBuffer, err := buffer(step)
