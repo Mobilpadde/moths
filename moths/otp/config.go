828cb19b51d50f090cf99bf22fda1e7e5fef8535
Mads Cordes <Mobilpadde@gmail.com>
2023-01-11 20:08:55 +0100

    Add validation of moths directly

--- moths/otp/config.go
+++ moths/otp/config.go
@@ -9,7 +9,11 @@ type OTP struct {
 	emojies emojies.Emojies
 }
 
-func (otp OTP) Validate(code string) bool {
+func (otp OTP) Validate(moth string) bool {
+	return otp.emojies.String() == moth
+}
+
+func (otp OTP) ValidateToken(code string) bool {
 	return otp.token == code
 }
 
--- moths/validate.go
+++ moths/validate.go
@@ -1,6 +1,34 @@
 package moths
 
-func (m *Moths) Validate(code string) bool {
+import (
+	"time"
+
+	"moths/moths/otp"
+)
+
+func (m *Moths) Validate(moth string) bool {
+	if len(moth)/otp.EmojiBytes != m.amount {
+		return false
+	}
+
+	token, err := m.getToken(false)
+	if err != nil {
+		return false
+	}
+
+	same, err := otp.NewOTP(token, m.amount, m.emojies)
+	if err != nil {
+		return false
+	}
+
+	// Bump the time for
+	// m.getToken(true)
+	m.time = m.time.Add(time.Since(m.time))
+
+	return same.Validate(moth)
+}
+
+func (m *Moths) ValidateToken(code string) bool {
 	token, err := m.getToken(false)
 	if err != nil {
 		return false
