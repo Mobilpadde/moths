12028c9d1ff4811de91b8917ce75707e03ae4fda
Mads Cordes <Mobilpadde@gmail.com>
2023-03-07 21:42:55 +0100

    Test to accommodate changes

--- token/code/new_test.go
+++ token/code/new_test.go
@@ -2,6 +2,7 @@ package code
 
 import (
 	"testing"
+	"time"
 
 	"github.com/Mobilpadde/moths/v4/token/emojies"
 )
@@ -11,16 +12,46 @@ func TestNewCode(t *testing.T) {
 	token := "000000"
 	correct := "😸😼😹😺😸😻"
 
-	code, err := NewCode(token, amount, emojies.CATS)
+	code, err := NewCode(token, amount, emojies.CATS, time.Time{}, time.Time{})
 	if err != nil {
 		t.Error("Expected to not return an error when creating new code:", err)
 	}
 
-	if code.token != token {
-		t.Error("Expected token to be", token, "not", code.token)
+	if code.Token() != token {
+		t.Error("Expected token to be", token, "not", code.Token())
 	}
 
-	if code.emojies.String() != correct {
-		t.Error("Expected code to be", correct, "not", code.emojies)
+	if code.String() != correct {
+		t.Error("Expected code to be", correct, "not", code.String())
+	}
+}
+
+func TestCreatedAt(t *testing.T) {
+	amount := 6
+	token := "000000"
+	createdAt := time.Date(0, 0, 0, 0, 0, 0, 0, time.UTC)
+
+	code, err := NewCode(token, amount, emojies.CATS, createdAt, time.Time{})
+	if err != nil {
+		t.Error("Expected to not return an error when creating new code:", err)
+	}
+
+	if code.CreatedAt().Compare(createdAt) != 0 {
+		t.Error("Expected to be created at", createdAt, "not", code.CreatedAt())
+	}
+}
+
+func TestExpiresAt(t *testing.T) {
+	amount := 6
+	token := "000000"
+	expiresAt := time.Date(0, 0, 0, 0, 0, 0, 0, time.UTC)
+
+	code, err := NewCode(token, amount, emojies.CATS, time.Time{}, expiresAt)
+	if err != nil {
+		t.Error("Expected to not return an error when creating new code:", err)
+	}
+
+	if code.ExpiresAt().Compare(expiresAt) != 0 {
+		t.Error("Expected to expire at", expiresAt, "not", code.ExpiresAt())
 	}
 }
--- token/code/validate_test.go
+++ token/code/validate_test.go
@@ -2,6 +2,7 @@ package code
 
 import (
 	"testing"
+	"time"
 
 	"github.com/Mobilpadde/moths/v4/token/emojies"
 )
@@ -11,13 +12,13 @@ func TestValidate(t *testing.T) {
 	token := "000000"
 	correct := "😸😼😹😺😸😻"
 
-	code, err := NewCode(token, amount, emojies.CATS)
+	code, err := NewCode(token, amount, emojies.CATS, time.Time{}, time.Time{})
 	if err != nil {
 		t.Error("Expected to not return an error when creating new code:", err)
 	}
 
 	if !code.Validate(correct) {
-		t.Error("Expected code to be", correct, "not", code.emojies)
+		t.Error("Expected code to be", correct, "not", code.String())
 	}
 }
 
@@ -25,12 +26,12 @@ func TestValidateToken(t *testing.T) {
 	amount := 6
 	token := "000000"
 
-	code, err := NewCode(token, amount, emojies.CATS)
+	code, err := NewCode(token, amount, emojies.CATS, time.Time{}, time.Time{})
 	if err != nil {
 		t.Error("Expected to not return an error when creating new code:", err)
 	}
 
 	if !code.ValidateToken(token) {
-		t.Error("Expected code to be", token, "not", code.token)
+		t.Error("Expected code to be", token, "not", code.Token())
 	}
 }
