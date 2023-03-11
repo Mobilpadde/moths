c47f9c5e0464e18c2ca96a5e23ec3000b26d1000
Mads Cordes <Mobilpadde@gmail.com>
2023-03-11 22:00:30 +0100

    Check will check for all options before trying to get a code

--- token/check.go
+++ token/check.go
@@ -1,11 +1,25 @@
 package token
 
+import "github.com/Mobilpadde/moths/v5/token/checks"
+
 // Check is checking if everything
 // is working
 //
 // Meaning no errors is generated
 // from getting codes
 func (g *Generator) Check() error {
+	if err := checks.CheckAmount(g.amount); err != nil {
+		return err
+	}
+
+	if err := checks.CheckEmojies(g.emojies); err != nil {
+		return err
+	}
+
+	if err := checks.CheckPeriod(g.period); err != nil {
+		return err
+	}
+
 	if _, err := g.getToken(); err != nil {
 		return err
 	}
