a1d70461813a07167c169a0d9ecbc9bb51f93e01
Mads Cordes <Mobilpadde@gmail.com>
2023-03-11 22:02:18 +0100

    * 🚨 test(token): move token package tests to _test.go files The tests for the token package have been moved to files with the suffix _test.go to follow the naming convention for Go test files. The package name has also been changed to token_test to match the new file names. The import statements have been updated to reflect the new package name and the new location of the Generator struct. The options used to create a new Generator instance have been moved to the option package.

--- token/next_test.go
+++ token/next_test.go
@@ -1,23 +1,25 @@
-package token
+package token_test
 
 import (
 	"strings"
 	"testing"
 	"time"
 
+	"github.com/Mobilpadde/moths/v5/token"
 	"github.com/Mobilpadde/moths/v5/token/emojies"
+	"github.com/Mobilpadde/moths/v5/token/option"
 )
 
 func TestNextAndValidate(t *testing.T) {
 	amount := 6
 	secret := strings.Repeat("a", 32)
 
-	gen, err := NewGenerator(
-		OptionWithSecret(secret),
-		OptionWithPeriod(time.Second),
-		OptionWithAmount(amount),
-		OptionWithEmojies(emojies.CATS),
-		OptionWithTime(time.Date(0, 0, 0, 0, 0, 0, 0, time.UTC)),
+	gen, err := token.NewGenerator(
+		option.OptionWithSecret(secret),
+		option.OptionWithPeriod(time.Second),
+		option.OptionWithAmount(amount),
+		option.OptionWithEmojies(emojies.CATS),
+		option.OptionWithTime(time.Date(0, 0, 0, 0, 0, 0, 0, time.UTC)),
 	)
 	if err != nil {
 		t.Error("Expected to not return an error when creating new generator:", err)
--- token/validate_test.go
+++ token/validate_test.go
@@ -1,11 +1,13 @@
-package token
+package token_test
 
 import (
 	"log"
 	"strings"
 	"time"
 
+	"github.com/Mobilpadde/moths/v5/token"
 	"github.com/Mobilpadde/moths/v5/token/emojies"
+	"github.com/Mobilpadde/moths/v5/token/option"
 )
 
 func ExampleGenerator_Validate() {
@@ -13,12 +15,12 @@ func ExampleGenerator_Validate() {
 	secret := strings.Repeat("a", 32)
 
 	var err error
-	var gen *Generator
-	if gen, err = NewGenerator(
-		OptionWithSecret(secret),
-		OptionWithPeriod(time.Second),
-		OptionWithAmount(amount),
-		OptionWithEmojies(emojies.CATS),
+	var gen *token.Generator
+	if gen, err = token.NewGenerator(
+		option.OptionWithSecret(secret),
+		option.OptionWithPeriod(time.Second),
+		option.OptionWithAmount(amount),
+		option.OptionWithEmojies(emojies.CATS),
 	); err != nil {
 		log.Fatalln(err)
 	}
